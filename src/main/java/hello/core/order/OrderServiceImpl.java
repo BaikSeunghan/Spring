1260b3b3e038929d1f44b9cee6cc4d5e51a45aed
BaikSeunghan <tmdgks103@naver.com>
2022-04-01 13:00:32 +0900

    컴포넌트 스캔 탐색 위치와 스캔대상, 필터(includeFilters,excludeFilters)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,10 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
@@ -12,6 +15,8 @@ public class OrderServiceImpl implements OrderService{
     //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     //    이렇게하면, DIP를 위반하게 되기때문에 구현부를 지운다.
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
