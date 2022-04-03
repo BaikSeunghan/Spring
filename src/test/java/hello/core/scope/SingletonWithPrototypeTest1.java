3780cd778456672197ebe8ffcd134b8700d9504b
BaikSeunghan <tmdgks103@naver.com>
2022-04-03 22:52:26 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 @ObjectProvider, @Provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,11 +1,13 @@
 package hello.core.scope;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import
-        org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class SingletonWithPrototypeTest1 {
@@ -18,19 +20,41 @@ public class SingletonWithPrototypeTest1 {
         assertThat(count1).isEqualTo(1);
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
+    // ObjectFactory: 기능이 단순, 별도의 라이브러리 필요 없음, 스프링에 의존
+    // ObjectProvider: ObjectFactory 상속, 옵션, 스트림 처리등 편의 기능이 많고, 별도의 라이브러리 필요 없음, 스프링에 의존
+    // 지정한 빈을 컨테이너에서 대신 찾아주는 DL(DependencyLookup) 서비스를
+    // 제공하는 것이 바로 ObjectProvider 이다
+    /*
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); // 항상 새로운 프로토타입 빈이 생성
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
         }
     }
+    */
+    // provider 의 get() 을 호출하면 내부에서는 스프링 컨테이너를 통해 해당 빈을 찾아서 반환한다. (DL)
+    // 자바 표준이므로 스프링이 아닌 다른 컨테이너에서도 사용할 수 있다.
+    static class ClientBean {
+
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); // 항상 새로운 프로토타입 빈이 생성
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
     @Scope("prototype")
     static class PrototypeBean {
         private int count = 0;
